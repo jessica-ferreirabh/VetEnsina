d24e08c5e22520182c4b41222e0b1dbe63abba82
leticiameireles9 <57921642+leticiameireles9@users.noreply.github.com>
2019-12-10 21:18:29 -0300

    adicionar mudancas

--- adestrador.js
+++ adestrador.js
@@ -1,7 +1,13 @@
+
+
 $(document).ready(function () {
+
     let data = JSON.parse(localStorage.getItem("adestradores"));
     console.log(data)
     mostrarinfo(data)
+    
+
+
 })
 
 function mostrarinfo (dbinfo){
@@ -12,36 +18,46 @@ function mostrarinfo (dbinfo){
     for (i=0;i < dbinfo.formul.length;i++){
         console.log(dbinfo.formul[i].nome)
         adestradores += `
-        <div class="adestrador">
-                <div class="foto">
-                <img src="${dbinfo.formul[i].urlimagem}" alt="">
-                </div>
-                
-                <div class="nome">
-                <h4> ${dbinfo.formul[i].nome}</h4>
-                </div>
+          <div class="adestrador">
 
-                <div class="sexo">
-                <h4> ${dbinfo.formul[i].sexo}</h4>
-                </div>
-                <div class="cidade">
-                <h4> ${dbinfo.formul[i].cidade}</h4>
-                </div>
-                <div class="estado">
-                <h4> ${dbinfo.formul[i].estado}</h4>
-                </div>
-                <div class="email">
-                <h4> ${dbinfo.formul[i].email}</h4>
-                </div>
-                <div class="especialização">
-                <h4> ${dbinfo.formul[i].especialização}</h4>
-                </div>
-                <div class="experiencia">
-                <h4> ${dbinfo.formul[i].experiencia}</h4>
-                </div>
-                </div>
-                        `
-        ;
+            <div class="foto">
+            <img src="${dbinfo.formul[i].imgData}" alt="">
+            </div>
+            
+            <div class="nome">
+            <h4> ${dbinfo.formul[i].nome}</h4>
+            </div>
+
+            <div class="sexo">
+            <h4> ${dbinfo.formul[i].sexo}</h4>
+            </div>
+
+            <div class="idade">
+            <h4> ${dbinfo.formul[i].idade}</h4>
+            </div>
+
+            <div class="cidade">
+            <h4> ${dbinfo.formul[i].cidade}</h4>
+            </div>
+
+            <div class="estado">
+            <h4> ${dbinfo.formul[i].estado}</h4>
+            </div>
+
+            <div class="email">
+            <h4> ${dbinfo.formul[i].email}</h4>
+            </div>
+
+            <div class="especialização">
+            <h4> ${dbinfo.formul[i].especializacao}</h4>
+            </div>
+
+            <div class="experiencia">
+            <h4> ${dbinfo.formul[i].experiencia} de experiência</h4>
+            </div>
+
+          </div>
+        `;
 
     }
     $("#conteudo").append(adestradores);
--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,22 @@
+var imgConvertedToBase64;
+
+document.getElementById('inputImage').addEventListener('change', function(event) {
+    
+    var myCanvas = document.createElement("canvas");
+    var ctx = myCanvas.getContext('2d');
+    var img = new Image();
+    img.src = '';
+    img.addEventListener('load',function() {});
+    img.onload = function(){
+        myCanvas.width = img.width;
+        myCanvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        var dataUrl = myCanvas.toDataURL('image/jpeg');
+        imgConvertedToBase64 = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+    };
+    img.src = URL.createObjectURL(event.target.files[0]);
+});
+
 $("#enviar").on("click", function () {
 
     var contents = []
@@ -6,30 +25,34 @@ $("#enviar").on("click", function () {
         contents[contents.length] = content[i].value
     };
 
+    console.log("contents");
+    console.log(contents);
+
     const nome = contents[0];
-    const idade = 23;
     const sexo = contents[1];
-    const cidade = contents[2];
-    const estado = contents[3];
-    const email = contents[5];
+    const idade = contents[2];
+    const cidade = contents[3];
+    const estado = contents[4];
+    const especializacao = contents[5];
+    const experiencia = contents[6];
+    const email = contents[7];
 
     const formulario = {
-
-        nome: nome,
-        idade: idade,
-        sexo: sexo,
-        cidade: cidade,
-        estado: estado,
-        email: email,
-        especialização: "",
-        experiencia: '4 Anos',
-        urlimagem: 'lud.jpg',
-
+      nome: nome,
+      idade: idade,
+      sexo: sexo,
+      cidade: cidade,
+      estado: estado,
+      email: email,
+      especializacao: especializacao,
+      experiencia: experiencia,
+      imgData:imgConvertedToBase64
     }
 
     let i = 0;
 
-    while (contents[i] !== "" && i < contents.length) i++;
+    while (contents[i] !== "" && i < contents.length) 
+      i++;
 
     if (i != contents.length)
         alert("Favor prencher todos os campos!")
@@ -39,12 +62,16 @@ $("#enviar").on("click", function () {
     }
 })
 
-
 function salvar(data) {
     let db = JSON.parse(localStorage.getItem("adestradores"));
+    
     db.formul.push(data);
 
     localStorage.setItem("adestradores", JSON.stringify(db))
 }
 
+function getBase64Image(image) {
+  
 
+    //return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
